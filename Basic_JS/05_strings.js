d6dbb3c14102b08605c474991788cc411ad974a4
zeman01 <deshadow95@gmail.com>
2025-12-07 08:05:05 +0545

    strings

---
+++ Basic_JS/05_strings.js
@@ -0,0 +1,34 @@
+const name = "jaman";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("jaman-hc-com");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("t"));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne = "   jaman    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://jaman.com/jaman%20choudhary";
+
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("santosh"));
+
+console.log(gameName.split("-"));
