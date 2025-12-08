c4a2539201bd01229060bbbaf05d089ec57cd127
zeman01 <deshadow95@gmail.com>
2025-12-08 12:23:32 +0545

    array and objects

---
+++ 02_Arrays_&_Objects/01_array.js
@@ -0,0 +1,67 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeroes = ["Iron Man", "Thor", "Spider Man"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("C ", myArr);
+console.log(myn2);
+
+const marvel_heroes = ["thor", "Iron man", "spider man"];
+const dc_heroes = ["superman", "flash", "batman"];
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+const all_new_heroes = [...marvel_heroes, ...dc_heroes];
+
+// console.log(all_new_heroes);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+console.log(Array.isArray("Jaman"));
+console.log(Array.from("Jaman"));
+console.log(Array.from({ name: "Jaman" })); // interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
